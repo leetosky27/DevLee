196e048ac75a00447e4dc44c7eb4816ea622fba1
leetosky27 <leetosky27@gmail.com>
2025-03-25 17:34:15 -0500

    primer programa

---
+++ Dart/conversion de datos/programa.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+void main(){
+  print("Por favor, introduce tu nombre:");
+  String? nombre = stdin.readLineSync();
+
+  // mostrar los datos capturados
+ // print ("Hola, $nombre!");
+  print("Hola, $nombre!");}
