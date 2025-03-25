5d73a666e1e9d99a18368b1560c5febc5cbbc6c5
leetosky27 <leetosky27@gmail.com>
2025-03-25 17:46:19 -0500

    funciones js

---
+++ JS/funciones.js
@@ -0,0 +1,17 @@
+
+function getRandomNumber() {
+    // recuperamos un número aleatorio entre 0 y 1
+    const random = Math.random() // por ejemplo: 0.6803487380457318
+
+    // lo multiplicamos por 10 para que esté entre 0 y 10
+    const multiplied = random * 10 // -> 6.803487380457318
+
+    // redondeamos hacia abajo para que esté entre 0 y 9
+    const rounded = Math.floor(multiplied) // -> 6
+
+    // le sumamos uno para que esté entre 1 y 10
+    const result = rounded + 1 // -> 7
+
+    // devolvemos el resultado
+    return result
+}
