f0988ecb867addefa29def80baaac850d8446ded
leetosky27 <leetosky27@gmail.com>
2025-03-25 17:46:26 -0500

    parametros js

---
+++ JS/parametros.js
@@ -0,0 +1,20 @@
+function saludar(usuario){
+    console.log(`Hola ${usuario}`)
+
+
+}
+function sumar (a,b){
+    return a + b
+}
+
+function cocinarMicroondas (plato, tiempo, potencia ){
+if (plato=== "pollo" && tiempo === 10 && potencia === 5){
+return "pollo"
+if (plato === "huevo" && tiempo === 2 && potencia ===3){
+    return "huevo frito"
+}
+
+}
+
+    return "invalido"
+}
