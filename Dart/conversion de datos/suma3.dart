f0f1329de6f21c2213e95de446334b28457deba0
leetosky27 <leetosky27@gmail.com>
2025-03-25 17:34:43 -0500

    suma de string y double

---
+++ Dart/conversion de datos/suma3.dart
@@ -0,0 +1,11 @@
+
+
+void main (){
+  String num3 = "5.9";
+  double num4= 3.5;
+  double sumaD;
+
+  double num3Convertido = double.parse(num3);
+  sumaD = num3Convertido + num4;
+  print(sumaD);
+}
